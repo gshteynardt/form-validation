dca22273891cc8f36a456ed624a523d0b61ba4dc
GermanShtey <gvshteynardt@gmail.com>
2020-12-09 18:27:48 +0300

    feat: создать форму, созадать компонент input

---
+++ src/UI/Input/Input.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import classes from './Input.module.css';
+
+
+const isInvalid = (valid, touched, shouldValidate) => {
+  return !valid && shouldValidate && touched
+}
+
+export const Input = ({ type, label, value, onChange, error, valid, touched, shouldValidate}) => {
+  const inputType = type || 'text';
+  const cls = [classes.input__wrapper];
+  const htmlFor = `${inputType}-${Math.random()}`;
+
+  if(isInvalid( valid, touched, shouldValidate)) {
+    cls.push(classes.input__wrapper_invalid)
+  }
+
+  return(
+    <div className={cls.join(' ')}>
+      <label htmlFor={htmlFor}>{label}</label>
+      <input
+        id={htmlFor}
+        type={inputType}
+        value={value}
+        onChange={onChange}
+        className={classes.input__input}
+      />
+
+      {
+        isInvalid( valid, touched, shouldValidate)
+          ? <span className={classes.input__span}>
+            {error || 'Введите верное значение'}
+        </span>
+          : null
+      }
+    </div>
+  )
+}
---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,131 @@
+import React, { useState } from 'react';
+import classes from './Auth.module.css';
+import Button from '../../UI/Button/Button';
+import { Input } from "../../UI/Input/Input";
+
+const  validateEmail = (inputEmail) => {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(inputEmail).toLowerCase());
+}
+
+export const Auth = () => {
+
+  const initValues = {
+    email: {
+      value: '',
+      type: 'email',
+      label: 'Email',
+      error: 'Введите корректный email',
+      valid: false,
+      touched: false,
+      validations: {
+        required: true,
+        email: true,
+      }
+    },
+    password: {
+      value: '',
+      type: 'password',
+      label: 'Password',
+      error: 'Введите корректный пароль',
+      valid: false,
+      touched: false,
+      validations: {
+        required: true,
+        minLength: 6,
+      }
+    }
+  }
+
+  const [formControls, setFormControls] = useState(initValues);
+
+  const handlerLogin = () => {
+
+  }
+
+  const handlerRegister = () => {
+
+  }
+
+  const handlerSubmit = () => {
+
+  }
+
+  //определяем валидный inputs
+  const validateControl = (value, validations) => {
+    //валидировать control не нужно
+    if(!validations) {
+      return true
+    }
+
+    let isValid = true;
+
+    if(validations.required) {
+      isValid = (value.trim() !== '') && isValid;
+    }
+
+    if(validations.email) {
+      isValid = validateEmail(value) && isValid;
+    }
+
+    if(validations.minLength) {
+      isValid = (value.length >= validations.minLength) && isValid;
+    }
+
+    return isValid;
+  }
+
+  //обработчик событий inputs
+  const onChangeHandler = (evt, controlName) => {
+    const control = {...formControls[controlName]};
+
+    control.value = evt.target.value;
+    control.touched = true;
+    control.valid = validateControl(control.value, control.validations);
+    setFormControls(prevState => ({prevState, ...control}));
+  }
+
+  //создаем inputs
+  const renderInputs = () => {
+    return Object.keys(formControls).map((controlName, index) => {
+      const control = formControls[controlName];
+      return <Input
+        key={controlName + index}
+        type={control.type}
+        value={control.value}
+        valid={control.valid}
+        touched={control.touched}
+        label={control.label}
+        shouldValidate={!!control.validations}
+        error={control.error}
+        onChange={e => onChangeHandler(e, controlName)}
+      />
+    })
+  }
+
+  return(
+    <div className={classes.auth}>
+      <div>
+        <h1 className={classes.auth__title}>Авторизация</h1>
+        <form
+          className={classes.auth__form}
+          onSubmit={handlerSubmit}
+        >
+          {renderInputs()}
+          <Button
+            type="success"
+            onClick={handlerLogin}
+          >
+            Войти
+          </Button>
+          <Button
+            type='success'
+            onClick={handlerRegister}
+          >
+            Авторизоваться
+          </Button>
+        </form>
+      </div>
+    </div>
+  )
+}
